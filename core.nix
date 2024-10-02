a31c94bc6f3a1d3ddaf1876bd4c97e5a1d9f49cd
Miku <mikuwayo@protonmail.com>
2024-10-02 21:22:35 +0200

    Configure neovim

--- core.nix
+++ core.nix
@@ -2,10 +2,31 @@
 {
 	nixpkgs.config.allowUnfree = true;
 	nix.settings.experimental-features = [ "nix-command" "flakes" ];
-	environment.shellAliases = { vim = "nvim"; };
+
 	environment.systemPackages = with pkgs; [
 		hello git tmux fish
-		neovim gnumake man-pages
+		gnumake man-pages
 		curl wget openssh openssl
 	];
+
+	programs.neovim = {
+		enable = true;
+		vimAlias = true;
+		defaultEditor = true;
+		configure = {
+			customRC = ''
+				set nocompatible
+				set showmatch
+				set hlsearch
+				set incsearch
+				set tabstop=2
+				set softtabstop=2
+				set expandtab
+				set shiftwidth=2
+				set autoindent
+				set number
+			'';
+		};
+	};
+
 }
