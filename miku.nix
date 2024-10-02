825ee123a4a9da40e165ccdcf6f6303a689fea2f
Miku <mikuwayo@protonmail.com>
2024-10-02 22:43:33 +0200

    Empty fish directories

--- miku.nix
+++ miku.nix
@@ -1,7 +1,35 @@
 { config, pkgs, ... }:
 {
-  home.username = "miku";
-  home.homeDirectory = "/home/miku";
+  home = {
+    username = "miku";
+    homeDirectory = "/home/miku";
+
+    packages = with pkgs; [
+
+    ];
+
+    file = {
+      "conf.d" = {
+        source = ./fish/conf.d;
+        target = ".config/fish/conf.d";
+        recursive = true;
+      };
+
+      "functions" = {
+        source = ./fish/functions;
+        target = ".config/fish/";
+        recursive = true;
+      };
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Miku";
+    userEmail = "mikuwayo@protonmail.com";
+  };
+
+  programs.emacs.enable = true;
 
   # No Touchy
   home.stateVersion = "24.05";
